6b5ebaff62eb2c355f68cb33d2dfa80bb4a71553
lev-belousov <htext@bk.ru>
2019-06-03 16:48:05 +0500

    Build test

---
+++ build/precache-manifest.70d9756329f9099404cb43dfa47ca62b.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = [
+  {
+    "revision": "b50f5872459e8e17eaffd5d57a2b2f8e",
+    "url": "/static/media/Montserrat-Thin.b50f5872.ttf"
+  },
+  {
+    "revision": "aededaac7fa4416e75e4",
+    "url": "/static/css/main.116cf91d.chunk.css"
+  },
+  {
+    "revision": "b1dd23817b3ffb0092f5bd4f470acb99",
+    "url": "/static/media/ibtn-menu.b1dd2381.svg"
+  },
+  {
+    "revision": "f46079f9e8b0c639f120",
+    "url": "/static/js/1.f46079f9.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/static/media/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "/static/media/MaterialIcons-Regular.e79bfd88.eot"
+  },
+  {
+    "revision": "28db829912bb402df5b1e6fb16f1908c",
+    "url": "/static/media/Framework7Icons-Regular.28db8299.eot"
+  },
+  {
+    "revision": "5fce7c1238bad823bee064bd127fe4cd",
+    "url": "/static/media/Framework7Icons-Regular.5fce7c12.woff"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/static/media/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "111371010ef70bbcf7e6968abc8e2fb2",
+    "url": "/static/media/Framework7Icons-Regular.11137101.ttf"
+  },
+  {
+    "revision": "19158ff09eb907432a7ccacd074ef9d5",
+    "url": "/static/media/Framework7Icons-Regular.19158ff0.woff2"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/static/media/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "f9ab30a63e119d88351c7bd8d87a8459",
+    "url": "/static/media/Montserrat-ExtraLightItalic.f9ab30a6.ttf"
+  },
+  {
+    "revision": "a8a117360e71de94ae3b0b0f8d15b44d",
+    "url": "/static/media/Montserrat-Regular.a8a11736.ttf"
+  },
+  {
+    "revision": "9b5089e2084e7a0a80e3ea8ad870a260",
+    "url": "/static/media/Montserrat-SemiBoldItalic.9b5089e2.ttf"
+  },
+  {
+    "revision": "b0cf52e5f2f3e3fbc204cb06fd0e7b11",
+    "url": "/static/media/Montserrat-Italic.b0cf52e5.ttf"
+  },
+  {
+    "revision": "aededaac7fa4416e75e4",
+    "url": "/static/js/main.aededaac.chunk.js"
+  },
+  {
+    "revision": "e3242149669bebf6afc4b0b5d22ec6ae",
+    "url": "/static/media/Montserrat-Black.e3242149.ttf"
+  },
+  {
+    "revision": "e375c6fe9bbeadb38d4911107e480378",
+    "url": "/static/media/Montserrat-ExtraBold.e375c6fe.ttf"
+  },
+  {
+    "revision": "5ba39d1f3eea1841e63a434e62e7f2d4",
+    "url": "/static/media/Montserrat-ThinItalic.5ba39d1f.ttf"
+  },
+  {
+    "revision": "9b9e2f4637ef72c726fda60e06b78d13",
+    "url": "/static/media/Montserrat-ExtraBoldItalic.9b9e2f46.ttf"
+  },
+  {
+    "revision": "2f354053d1c9c36f533ebd226698bfa5",
+    "url": "/static/media/Montserrat-Light.2f354053.ttf"
+  },
+  {
+    "revision": "0098f804fc2d06af52650e0b8ed3390c",
+    "url": "/static/media/Montserrat-Medium.0098f804.ttf"
+  },
+  {
+    "revision": "3eb3e92074690a8bb2dc9f11a67d4a80",
+    "url": "/static/media/Montserrat-LightItalic.3eb3e920.ttf"
+  },
+  {
+    "revision": "215a9e631fb43de893093e70db016cff",
+    "url": "/static/media/Montserrat-BoldItalic.215a9e63.ttf"
+  },
+  {
+    "revision": "94603128108f9cf9b26626564265c1d0",
+    "url": "/static/media/Montserrat-ExtraLight.94603128.ttf"
+  },
+  {
+    "revision": "97573fc844f3f38e77c36bdc9ed07cbb",
+    "url": "/static/media/Montserrat-MediumItalic.97573fc8.ttf"
+  },
+  {
+    "revision": "a3b387c93882604792867736aecd56c8",
+    "url": "/static/media/Montserrat-Bold.a3b387c9.ttf"
+  },
+  {
+    "revision": "bbcd5bbb5993808a92df01a5dfef77cd",
+    "url": "/static/media/Montserrat-SemiBold.bbcd5bbb.ttf"
+  },
+  {
+    "revision": "59a08c76185220c732ed4926d32a6706",
+    "url": "/static/media/Montserrat-BlackItalic.59a08c76.ttf"
+  },
+  {
+    "revision": "3e33629bddbec829455955f7c383089a",
+    "url": "/static/media/logo.3e33629b.svg"
+  },
+  {
+    "revision": "f46079f9e8b0c639f120",
+    "url": "/static/css/1.19d81570.chunk.css"
+  },
+  {
+    "revision": "bc0d5a066b0e70e65e5fbed708f845a3",
+    "url": "/index.html"
+  }
+];
